15f6087d24d800d6267cf630aec85f913f7f829a
lukas <ryntluka@fit.cvut.cz>
2021-03-03 23:14:16 +0100

    Stem and async document loading

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Imports
 const express = require('express');
+const load = require('./load');
+
 const app = express();
 
 // Import routes
 app.get('/', (req, res) => {
     res.send("To be implemented...");
 })
+
+// Stem documents
+load();
 
 // Listen to port
 const port = process.env.PORT || 3000;
