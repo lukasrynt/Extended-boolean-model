1d06e5fdb2a00a5e654cfc9b7f88bd24890f9a89
Lukas Rynt <ryntluka@fit.cvut.cz>
2021-03-30 23:58:56 +0200

    Add summary to the bottom of the page

--- client/src/components/FileList.js
+++ client/src/components/FileList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import File from './File'
 import '../App.css';
 
@@ -8,8 +8,9 @@ const FileList = ( {pageNumber, data} ) => {
             {data.slice(5 * (pageNumber), 5 * (pageNumber) + 5).map(file =>(
                 <File id={file.file} key={file.file.slice(0, -4)} text={file.content} data={data}/>
             ))}
+            <p className="file-count">Showing {data.slice(5 * (pageNumber), 5 * (pageNumber) + 5).length} out of {data.length} entries</p>
         </div>
-    )
+)
 };
 
 export default FileList;
